f89b1fbc6d76d74e5a69ea35eb99aa27732072d0
Dankku <dude.dankku@gmail.com>
2021-04-13 11:17:09 +0300

    Teleporting scene created
    - new scene
    -teleporting system with player created
    -hand tracking implemented for teleportation
    -OVRCameraRig is "player"

---
+++ SGP-HummingBird-GP3/Assets/Scripts/RayActivation.cs
@@ -0,0 +1,20 @@
+﻿using OculusSampleFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayActivation : MonoBehaviour
+{
+    [SerializeField] Interactable interactable;
+    [SerializeField] Transform playerTransform;
+    private void Start()
+    {
+        interactable.ActionZoneEvent += PrintMe;
+    }
+
+    public void PrintMe(ColliderZoneArgs args)
+    {
+        if (args.InteractionT == InteractionType.Enter)
+            playerTransform.position = transform.position;
+    }
+}
