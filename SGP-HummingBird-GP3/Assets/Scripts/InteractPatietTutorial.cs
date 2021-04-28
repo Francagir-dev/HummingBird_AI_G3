1745885638441fdaeecffb130f1b5fb6d1d2282c
Francisco Carrasco <francagir@hotmail.com>
2021-04-28 12:16:11 +0300

    Main menu => Tutorial => Main Game

---
+++ SGP-HummingBird-GP3/Assets/Scripts/InteractPatietTutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractPatietTutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject patientCanvasInfo;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.gameObject.CompareTag("Player"))
+        {
+            patientCanvasInfo.SetActive(true);
+            StartCoroutine(HideCanvas());
+        }
+    }
+
+    IEnumerator HideCanvas()
+    {
+        patientCanvasInfo.SetActive(false);
+        yield return new WaitForSeconds(2);
+    }
+}
---
+++ SGP-HummingBird-GP3/Assets/Scripts/TutorialButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialButtons : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Scenes/MenuScene");
+    }
+
+    public void GoToHospital()
+    {
+        SceneManager.LoadScene("Scenes/HospitalRoom");
+    }
+}
